538e5a567dcb3bc56c8034b0ae044767ad6cd84c
Tony Hirst <tony.hirst@gmail.com>
2023-08-08 13:21:45 +0100

    Update build_pdf_docx.sh

--- build_pdf_docx.sh
+++ build_pdf_docx.sh
@@ -3,22 +3,53 @@
 # Script for building output documents
 # from sphinx/Jupyter Book souirce documents
 
+# TOC sand config files in buildpack/MODULE
+# Run as something like:
+# ./build_pdf_docx.sh default
+# Outputs in: buildpack/MODULE/_generated
+
+MODULE=${1:-default}
+CONFIG=${2:-_config.yml}
+TOC=${3:-_toc.yml}
+OUT=${4:-./_generated}
+
+BUILDPACK=buildpack/$MODULE
+echo "Using config etc. in: $BUILDPACK"
+echo "- config file: $CONFIG"
+echo "- toc file: $TOC"
+echo "- outputh path: $OUT"
+
+cp -f $BUILDPACK/$TOC .
+cp -f $BUILDPACK/$CONFIG .
+
+rm -r $BUILDPACK/$OUT/
+mkdir -p $BUILDPACK/$OUT/
+
+JB_BUILD="jb build . --config $CONFIG --toc $TOC" # --path-output $OUT"
+
+echo "Using base command: $JB_BUILD"
+
+# Default build of HTML book
+$JB_BUILD
+
 # We can build a single PDF as a jupyter-book build command
 # The pdflatex builder gives the nicest output so far
-jb build . --builder pdflatex #builds to _build/latex
+$JB_BUILD --builder pdflatex #builds to _build/latex
 # The pdfhtml builder requires pyppeteer; naive install errors in container
 #jb build . --builder pdfhtml #builds to _build/pdf
 
 
 # XML build
-jb build --builder custom --custom-builder xml . #builds to _build/xml
+$JB_BUILD  --builder custom --custom-builder xml #builds to _build/xml
+
+
 # OU-XML conversion
 obt convert-to-ouxml . #builds to _build/ouxml
 
 # We can also generate output docs from the singlehtml doc
 # Source file - generated from jb singlehtml builder
-SINGLE_PAGE_HTML="root.html"
-jupyter-book build .  --builder singlehtml #builds to _build/singlehtml
+SINGLE_PAGE_HTML="index.html"
+$JB_BUILD --builder singlehtml #builds to _build/singlehtml
 
 cd _build/singlehtml
 
@@ -28,4 +59,12 @@ sed -i.bak 's/<a class="reference internal" href="#[^>]*"><span class="std std-n
 
 pandoc -o tesguide_single_doc.pdf "$SINGLE_PAGE_HTML" # in _build/singlehtml
 pandoc -o tesguide_single_doc.docx "$SINGLE_PAGE_HTML" #in _build/singlehtml
+
+cd ../..
+
+cp -rf _build/html $BUILDPACK/$OUT/
+cp -rf _build/ouxml $BUILDPACK/$OUT/
+cp _build/latex/book.pdf $BUILDPACK/$OUT/$MODULE.pdf
+
+ou_nb_brandify -o $BUILDPACK/$OUT -y 2023
 
